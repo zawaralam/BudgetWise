b1e6656b62b6241479f0456e6f40b506f31929f1
vincentyeungg <vincentyeung0809@gmail.com>
2021-03-28 01:21:54 -0400

    merge conflicts

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,18 @@ var express = require('express');
 var router = express.Router();
 var db = require("../db");
 
-router.get('/', async function(req, res){
+router.get('/login', async function(req, res){
   res.render('login', { title: 'Login'})
 });
 
 
-router.post('/', async function(req, res){
+router.post('/login', async function(req, res){
   var { username, password,register} = req.body;
 
   if(register){
     res.render('register', { title: 'Register'})}
   else{
     await db.login(username, password);
-<<<<<<< HEAD
   }
 
   req.session.username = username;
@@ -28,11 +27,6 @@ function ensureLoggedIn(req, res, next){
     next();
   }
 }
-=======
-    req.session.username = username;
-    res.render('index', { title: 'Transactions'})}
-  });
->>>>>>> origin/main
 
   router.post('/register', async function(req, res){
     var { username, password,firstname, lastname, register} = req.body;
@@ -42,12 +36,6 @@ function ensureLoggedIn(req, res, next){
     res.redirect('/');
   });
 
-<<<<<<< HEAD
-router.get('/', async function(req, res){
-  var { username } = req.session;
-  res.render('index', { 
-    username,
-=======
  function ensureLoggedIn(req, res, next){
    if(!req.session.username){
      res.redirect('/login');
@@ -63,7 +51,6 @@ router.get('/', async function(req, res){
      username,
      items: await db.getTransaction(username),
    });
->>>>>>> origin/main
   });
 
  router.post('/reportview', async function(req, res){
